622b738a6bc9ad1d93ae13ea71dae222d33ffefe
dmn28 <damjantzar@gmail.com>
2023-02-25 21:37:25 +0100

    convert Customer and Fraud services to EurekaClients

--- fraud/src/main/java/com/dmn/fraud/FraudApplication.java
+++ fraud/src/main/java/com/dmn/fraud/FraudApplication.java
@@ -2,8 +2,10 @@ package com.dmn.fraud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
     public static void main(String[] args){
         SpringApplication.run(FraudApplication.class, args);
